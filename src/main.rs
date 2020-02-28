644986e8326c80df0bf5475c5e7fa630ccb656ef
McM1k-home <gboudrie@student.42.fr>
2020-02-28 18:53:18 +0100

    l192 https://github.com/vulkano-rs/vulkano-examples/blob/master/src/bin/triangle.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use vulkano::instance::{Instance, PhysicalDevice};
+
+use vulkano_win::VkSurfaceBuild;
+use winit::window::WindowBuilder;
+use vulkano::device::{DeviceExtensions, Device};
+use vulkano::swapchain::{Swapchain, SurfaceTransform, PresentMode, ColorSpace, FullscreenExclusive};
+use std::intrinsics::atomic_and;
+use vulkano::buffer::{CpuAccessibleBuffer, BufferUsage};
+
 fn main() {
-    println!("Hello, world!");
+    let instance = {
+        let extensions = vulkano_win::required_extensions();
+        Instance::new(None, &extensions, None).unwrap()
+    };
+
+    let physical = PhysicalDevice::enumerate(&instance).next().unwrap();
+    println!("Using device: {} (type: {:?})", physical.name(), physical.ty());
+
+    let mut events_loop = EventsLoop::new();
+    let surface = WindowBuilder::new().build_vk_surface(&events_loop, instance.clone()).unwrap();
+    let window = surface.window();
+
+    let queue_family = physical.queue_families().find(|&q| {
+        q.supports_graphics() && surface.is_supported(q).unwrap_or(false)
+    }).unwrap();
+
+    let device_ext = DeviceExtensions { khr_swapchain: true, .. DeviceExtensions::none()};
+    let (device, mut queues) = Device::new(physical, physical.supported_features(), &device_ext, [(queue_family, 0.5)].iter().cloned()).unwrap();
+
+    let queue = queues.next().unwrap();
+
+    let (mut swapchain, images) = {
+        let caps = surface.capabilities(physical).unwrap();
+        let usage = caps.supported_usage_flags;
+        let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+        let format = caps.supported_formats[0].0;
+        let initial_dimensions = if let Some(dimensions) = window.get_inner_size() {
+            let dimensions: (u32, u32) = dimensions.to_physical(window.get_hidpi_factor()).into();
+            [dimensions.0, dimensions.1]
+        } else {
+            return;
+        };
+
+        Swapchain::new(device.clone(), surface.clone(), caps.min_image_count, format,
+            initial_dimensions, 1, usage, &queue, SurfaceTransform::Identity, alpha,
+            PresentMode::Fifo, FullscreenExclusive::Default, false, ColorSpace::SrgbNonLinear).unwrap()
+    };
+
+    let vertex_buffer = {
+        #[derive(Default, Debug, Clone)]
+        struct Vertex { position: [f32; 2] }
+        vulkano::impl_vertex!(Vertex, position);
+
+        CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(), false, [
+            Vertex { position: [-0.5, -0.25] },
+            Vertex { position: [0.0, 0.5] },
+            Vertex { position: [0.25, -0.1] }
+        ].iter().cloned()).unwrap()
+    };
+
+
 }
+
+
